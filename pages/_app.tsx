ed8e720a70bbce7a9a03b71e5265b85835e56f56
JaeHun Kim <kkaa81@naver.com>
2022-05-04 19:23:49 +0900

    nav바 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,24 @@
 import type { AppProps } from 'next/app';
+import { useDispatch } from 'react-redux';
 
 import '../styles/globals.css';
+import { onDark } from '../store/actions/themeMode';
 import { wrapper } from '../store';
 import ThemeButton from '../components/ThemeButton';
+import Nav from '../components/Nav';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const dispatch = useDispatch();
+
+  if (typeof window !== 'undefined' && window.localStorage.getItem('mode') === 'dark') dispatch(onDark());
+
   return (
     <>
-      <Component {...pageProps} />
+      <Nav />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
       <ThemeButton />
     </>
   )
