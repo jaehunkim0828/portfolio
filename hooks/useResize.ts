af03b1f2191ff47c63656a7d90a0c751aa25b310
JaeHun Kim <kkaa81@naver.com>
2022-05-04 19:25:28 +0900

    custom hook: web size

---
+++ hooks/useResize.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+interface Size {
+  nowWidth: number;
+  nowHeight: number;
+}
+
+export function useResize(): Size {
+  const [windowSize, setWindowSize] = useState<Size>({
+    nowWidth: 0,
+    nowHeight: 0,
+  });
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        nowWidth: window.innerWidth,
+        nowHeight: window.innerHeight,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return windowSize;
+}
