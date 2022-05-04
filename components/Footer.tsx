7852f87a7f9fd9bf9ca043937b5b607a59493450
JaeHun Kim <kkaa81@naver.com>
2022-05-04 19:20:54 +0900

    create Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+import style from '../styles/footer.module.scss';
+
+
+export default function Footer() {
+    return (
+        <footer className={style.footer}>COPYRIGHT ⓒ 2022 웹개발자 김재훈. ALL RIGHTS RESERVED.</footer>
+    )
+}
